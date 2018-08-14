100510e22fca630a505e29509179ff402d6de6ad
Sean Kagawa <seankagawa@gmail.com>
2018-08-13 19:40:01 -1000

    compl Lesson 3, Module 21, Question 2: ORDER BY Part II

--- lesson03.sql
+++ lesson03.sql
@@ -19,13 +19,20 @@ SELECT id, account_id, total_amt_usd
 SELECT id, account_id, total
 	FROM orders
 	ORDER BY total
-	LIMIT 20
+	LIMIT 20;
 
 --21. %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 /*Write a query that returns the top 5 rows from orders ordered according to newest to oldest, but with the largest total_amt_usd for each date listed first for each date.*/
 
-SELECT total_amt_usd, occurred_at
+SELECT *
 	FROM orders
-	ORDER BY occurred_at DESC
-	LIMIT 5
+	ORDER BY occurred_at DESC, total_amt_usd DESC
+	LIMIT 5;
+
+/*Write a query that returns the top 10 rows from orders ordered according to oldest to newest, but with the smallest total_amt_usd for each date listed first for each date.*/
+
+SELECT *
+	FROM orders
+	ORDER BY occurred_at, total_amt_usd
+	LIMIT 10;
