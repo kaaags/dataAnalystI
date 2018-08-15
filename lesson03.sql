419f669a829a4854bb320d830a7dbd2c26da0587
Sean Kagawa <seankagawa@gmail.com>
2018-08-14 20:38:11 -1000

    compl L3, M39, Q1-3: NOT

--- lesson03.sql
+++ lesson03.sql
@@ -109,3 +109,23 @@ SELECT name, primary_poc, sales_rep_id
 SELECT *
 	FROM web_events
 	WHERE channel IN ('organic', 'adwords');
+
+--39. %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+/*1. Use the accounts table to find the account name, primary poc, and sales rep id for all stores except Walmart, Target, and Nordstrom.*/
+
+SELECT name, primary_poc, sales_rep_id
+	FROM accounts
+	WHERE name NOT IN ('Walmart', 'Target', 'Nordstrom');
+
+/*2. All companies whose names do not contain the string 'one' somewhere in the name.*/
+
+SELECT name
+	FROM accounts
+	WHERE name NOT LIKE ('%one%');
+
+/*3. All companies whose names do not end with 's'.*/
+
+SELECT name
+	FROM accounts
+	WHERE name NOT LIKE ('%s');
